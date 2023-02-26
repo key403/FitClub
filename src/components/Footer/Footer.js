fb127e2808e2d33a8daed20042192c9aab49de46
key403 <m.saenzz2311@gmail.com>
2023-02-25 21:08:09 -0300

    first commit

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,31 +1,36 @@
-import React from 'react'
-import "./Footer.css"
-import Github from "../../assets/github.png"
-import Instagram from "../../assets/instagram.png"
-import LinkedIn from "../../assets/linkedin.png"
-import Logo from "../../assets/logo.png"
+import React from "react";
+import "./Footer.css";
+import Github from "../../assets/github.png";
+import Instagram from "../../assets/instagram.png";
+import LinkedIn from "../../assets/linkedin.png";
+import Logo from "../../assets/logo.png";
 
 const Footer = () => {
   return (
-    <div className='Footer-container'>
+    <div className="Footer-container">
       <hr />
       <div className="footer">
         <div className="social-links">
-          <a href="https://github.com/key403"><img src={Github} alt="" /></a>
+          <a
+            href="https://github.com/key403"
+            alt="github"
+            className="social-links"
+          >
+            <img src={Github} alt="" />
+          </a>
           <img src={Instagram} alt="instagram" />
-          <img src={LinkedIn} alt="" />
+          <img src={LinkedIn} alt="linkedin" />
         </div>
-       
+
         <div className="logo-f">
           <img src={Logo} alt="" />
         </div>
-       
       </div>
 
       <div className="blur blur-f-1"></div>
       <div className="blur blur-f-2"></div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
